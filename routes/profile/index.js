82f47070fa520760d607c933eb6a0b1480432af4
Christian Fei <crifei93@gmail.com>
2017-05-26 04:50:06 +0200

    cleanup profile

--- routes/profile/index.js
+++ routes/profile/index.js
@@ -1,5 +1,5 @@
-import { h, Component } from 'preact';
-import style from './style';
+import { h, Component } from 'preact'
+import style from './style'
 
 const initialState = {
   time: Date.now(),
@@ -10,31 +10,31 @@ export default class Profile extends Component {
   constructor () {
     super()
 
-    this.state = initialState;
+    this.state = initialState
   }
 
   // gets called when this route is navigated to
-  componentDidMount() {
+  componentDidMount () {
     // start a timer for the clock:
-    this.timer = setInterval(() => this.updateTime(), 1000);
+    this.timer = setInterval(() => this.updateTime(), 1000)
   }
 
   // gets called just before navigating away from the route
-  componentWillUnmount() {
-    clearInterval(this.timer);
+  componentWillUnmount () {
+    clearInterval(this.timer)
   }
 
   // update the current time
   updateTime () {
-    this.setState({ time: Date.now() });
+    this.setState({ time: Date.now() })
   };
 
   increment () {
-    this.setState({ count: this.state.count+1 });
+    this.setState({ count: this.state.count + 1 })
   };
 
   // Note: `user` comes from the URL, courtesy of our router
-  render({ user }, { time, count }) {
+  render ({ user }, { time, count }) {
     return (
       <div class={style.profile}>
         <h1>Profile: {user}</h1>
@@ -48,6 +48,6 @@ export default class Profile extends Component {
           Clicked {count} times.
         </p>
       </div>
-    );
+    )
   }
 }
