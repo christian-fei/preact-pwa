60fc29e49f757089b680a6ae099f69c68335fdab
Christian Fei <crifei93@gmail.com>
2017-05-26 04:44:40 +0200

    header with constructor and initialState

--- routes/profile/index.js
+++ routes/profile/index.js
@@ -1,47 +1,53 @@
 import { h, Component } from 'preact';
 import style from './style';
 
+const initialState = {
+  time: Date.now(),
+  count: 10
+}
+
 export default class Profile extends Component {
-	state = {
-		time: Date.now(),
-		count: 10
-	};
+  constructor () {
+    super()
 
-	// gets called when this route is navigated to
-	componentDidMount() {
-		// start a timer for the clock:
-		this.timer = setInterval(this.updateTime, 1000);
-	}
+    this.state = initialState;
+  }
 
-	// gets called just before navigating away from the route
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	}
+  // gets called when this route is navigated to
+  componentDidMount() {
+    // start a timer for the clock:
+    this.timer = setInterval(this.updateTime, 1000);
+  }
 
-	// update the current time
-	updateTime = () => {
-		this.setState({ time: Date.now() });
-	};
+  // gets called just before navigating away from the route
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
 
-	increment = () => {
-		this.setState({ count: this.state.count+1 });
-	};
+  // update the current time
+  updateTime () {
+    this.setState({ time: Date.now() });
+  };
 
-	// Note: `user` comes from the URL, courtesy of our router
-	render({ user }, { time, count }) {
-		return (
-			<div class={style.profile}>
-				<h1>Profile: {user}</h1>
-				<p>This is the user profile for a user named { user }.</p>
+  increment () {
+    this.setState({ count: this.state.count+1 });
+  };
 
-				<div>Current time: {new Date(time).toLocaleString()}</div>
+  // Note: `user` comes from the URL, courtesy of our router
+  render({ user }, { time, count }) {
+    return (
+      <div class={style.profile}>
+        <h1>Profile: {user}</h1>
+        <p>This is the user profile for a user named { user }.</p>
 
-				<p>
-					<button onClick={this.increment}>Click Me</button>
-					{' '}
-					Clicked {count} times.
-				</p>
-			</div>
-		);
-	}
+        <div>Current time: {new Date(time).toLocaleString()}</div>
+
+        <p>
+          <button onClick={this.increment}>Click Me</button>
+          {' '}
+          Clicked {count} times.
+        </p>
+      </div>
+    );
+  }
 }
