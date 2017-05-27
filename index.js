da7907c5fa2f56deead473ba866fc255315ec60b
Christian Fei <crifei93@gmail.com>
2017-05-27 10:45:36 +0200

    sw

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-import './style';
+import './style'
 
-import { h } from 'preact';
-import { Router } from 'preact-router';
+import { h } from 'preact'
+import { Router } from 'preact-router'
 
-import Header from './components/header';
-import Home from './routes/home';
-import Profile from './routes/profile';
+import Header from './components/header'
+import Home from './routes/home'
+import Profile from './routes/profile'
+
+if (global.navigator && 'serviceWorker' in global.navigator) {
+	console.log('adding serviceWorker')
+  global.navigator.serviceWorker.register('/sw.js').then(function () {
+    console.log('Service Worker Registered')
+  })
+}
 
 export default () => (
-	<div id="app">
-		<Header />
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-		</Router>
-	</div>
-);
+  <div id='app'>
+    <Header />
+    <Router>
+      <Home path='/' />
+      <Profile path='/profile/' user='me' />
+      <Profile path='/profile/:user' />
+    </Router>
+  </div>
+)
